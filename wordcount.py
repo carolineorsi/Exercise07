6f2ef6ff2f12f87f42a4ae152d15cc11c9ca3fc7
Caroline Orsi <caroline.orsi@gmail.com>
2014-10-06 20:04:13 -0700

    Added function definitions.

--- wordcount.py
+++ wordcount.py
@@ -2,24 +2,44 @@ from sys import argv
 
 script, filename = argv
 
-word_list = []
+def create_word_list(filename):
 
-input_file = open(filename)
+	word_list = []
 
-for line in input_file:
-    words = line.rstrip().split(" ")
-    for word in words:
-        word_list.append(word)
+	input_file = open(filename)
 
-input_file.close()
+	for line in input_file:
+	    words = line.rstrip().split(" ")
+	    for word in words:
+	        word_list.append(word)
 
-word_count = {}
+	input_file.close()
 
-for item in word_list:
-    if word_count.get(item):
-        word_count[item] += 1
-    else:
-        word_count[item] = 1
+	return word_list
 
-for k, v in word_count.iteritems():
-    print "%s %d" % (k, v)
+
+def create_dictionary(input_list):
+
+	word_count = {}
+
+	for item in input_list:
+	    if word_count.get(item):
+	        word_count[item] += 1
+	    else:
+	        word_count[item] = 1
+
+	return word_count
+
+
+def print_dict(input_dict):
+
+	for key, value in input_dict.iteritems():
+	    print "%s %d" % (key, value)
+
+
+def main():
+	print_dict(create_dictionary(create_word_list(filename)))
+
+
+if __name__ == "__main__":
+	main()
